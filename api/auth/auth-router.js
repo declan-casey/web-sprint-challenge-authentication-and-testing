8651986538781f2040385ef24c025196ee140dfe
declan-casey <dpcaseyboy99@gmail.com>
2021-03-26 19:33:21 -0400

    Turn in

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const restricted = require('../middleware/restricted')
+const Jokes = require ('../jokes/jokes-model')
+
 
 router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+  // res.end('implement register, please!');
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -27,6 +32,26 @@ router.post('/register', (req, res) => {
     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
+      const credentials = req.body
+      if (isValid(credentials)) {
+        const rounds = process.env.BCRYPT_ROUNDS || 8;
+    
+        const hash = bcryptjs.hashSync(credentials.password, rounds);
+    
+        credentials.password = hash;
+    
+        Users.add(credentials)
+          .then(user => {
+            res.status(201).json({ data: user });
+          })
+          .catch(error => {
+            res.status(500).json({ message: error.message });
+          });
+      } else {
+        res.status(400).json({
+          message: "please provide username and password and the password shoud be alphanumeric",
+        });
+      }
 });
 
 router.post('/login', (req, res) => {
@@ -54,6 +79,43 @@ router.post('/login', (req, res) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
+      const { username, password } = req.body;
+
+      if (isValid(req.body)) {
+        Users.findBy({ username: username })
+          .then(([user]) => {
+            // compare the password the hash stored in the database
+            if (user && bcryptjs.compareSync(password, user.password)) {
+              // issue token
+              const token = buildToken(user)
+              res.status(200).json({ message: "Welcome to our API", token });
+            } else {
+              res.status(401).json({ message: "Invalid credentials" });
+            }
+          })
+          .catch(error => {
+            res.status(500).json({ message: error.message });
+          });
+      } else {
+        res.status(400).json({
+          message: "please provide username and password and the password shoud be alphanumeric",
+        });
+      }
 });
+
+function buildToken(user) {
+  const payload = {
+    // claims
+    subject: user.id,
+    username: user.username,
+    role: user.role,
+  }
+  const config = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(
+    payload, jwtSecret, config
+  )
+}
 
 module.exports = router;
